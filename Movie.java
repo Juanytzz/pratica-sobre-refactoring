813d723636c927905de9fb30e91d69ba623ed9ba
Juanytzz <juan20051105@gmail.com>
2024-11-24 17:44:36 -0300

    Commit 9

--- Movie.java
+++ Movie.java
@@ -7,6 +7,9 @@ public class Movie {
     private String _title;
     private int _priceCode;
 
+    private Movie _movie;
+    private int _daysRented;
+
     public Movie(String title, int priceCode) {
         _title = title;
         _priceCode = priceCode;
@@ -23,4 +26,32 @@ public class Movie {
     public String getTitle() {
         return _title;
     }
+
+    public int getDaysRented() {
+        return _daysRented;
+    }
+
+    public Movie getMovie() {
+        return _movie;
+    }
+
+   public double getCharge(int daysRented){
+    double thisAmount = 0;
+    switch (getMovie().getPriceCode()) {
+        case Movie.REGULAR:
+            thisAmount += 2;
+            if (getDaysRented() > 2)
+                thisAmount += (getDaysRented() - 2) * 1.5;
+            break;
+        case Movie.NEW_RELEASE:
+            thisAmount += getDaysRented() * 3;
+            break;
+        case Movie.CHILDRENS:
+            thisAmount += 1.5;
+            if (getDaysRented() > 3)
+                thisAmount += (getDaysRented() - 3) * 1.5;
+            break;
+    }
+    return thisAmount;
+   }
 }
