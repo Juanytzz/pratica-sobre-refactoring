57ef6521b88f0725486a5566fcd7767f3c14c203
Juanytzz <juan20051105@gmail.com>
2024-11-24 17:57:20 -0300

    Commit 11

--- Movie.java
+++ Movie.java
@@ -5,23 +5,36 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
 
     private String _title;
-    private int _priceCode;
+    private Price _price;
 
     private Movie _movie;
     private int _daysRented;
 
-    public Movie(String title, int priceCode) {
-        _title = title;
-        _priceCode = priceCode;
-    }
+    public class RegularPrice extends Price {
+        public int getPriceCode() {
+            return Movie.REGULAR;
+        }
+     }
 
-    public int getPriceCode() {
-        return _priceCode;
-    }
+     public int getPriceCode() {
+        return _price.getPriceCode();
+     }
 
-    public void setPriceCode(int arg) {
-        _priceCode = arg;
-    }
+     public void setPriceCode(int arg) {
+        switch (arg) {
+           case REGULAR:
+              _price = new RegularPrice();
+              break;
+           case CHILDRENS:
+              _price = new ChildrensPrice();
+              break;
+           case NEW_RELEASE:
+              _price = new NewReleasePrice();
+              break;
+           default:
+              throw new IllegalArgumentException("Incorrect Price Code");
+        }
+     }
 
     public String getTitle() {
         return _title;
