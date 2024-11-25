75e376778d90ba04719ae223a79255b81c0fe8bf
Juanytzz <juan20051105@gmail.com>
2024-11-24 22:19:32 -0300

    Commit 17 e 18

--- Statement.java
+++ Statement.java
@@ -1,4 +1,19 @@
-public class Statement {
+import java.util.Enumeration;
+
+public abstract class Statement {
     
-    
+    public String value(Customer aCustomer) {
+        String result = getHeader(aCustomer);
+        Enumeration rentals = aCustomer.getRentals();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += getRentalLine(each);
+        }
+        result += getFooter(aCustomer);
+        return result;
+    }
+
+    protected abstract String getHeader(Customer aCustomer);
+    protected abstract String getRentalLine(Rental each);
+    protected abstract String getFooter(Customer aCustomer);
 }
